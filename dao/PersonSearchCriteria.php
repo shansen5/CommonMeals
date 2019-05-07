972950ae24569844ff0d1094c47a60c5566aeeb9
Steve Hansen <steven.e.hansen@gmail.com>
2019-05-07 14:47:59 -0700

    Initial commit.

---
+++ dao/PersonSearchCriteria.php
@@ -0,0 +1,82 @@
+<?php
+
+
+/**
+ * Search criteria for {@link PersonDao}.
+ * <p>
+ * Can be easily extended without changing the {@link PersonDao} API.
+ */
+final class PersonSearchCriteria {
+    private $search_date = null;
+    private $person_id = null;
+    private $first_name = null;
+    private $last_name = null;
+    private $occupant_type = null;
+    private $unit_id = null;
+    private $show_all = true;
+
+    public function hasFilter() {
+        if ( $this->search_date || $this->unit_id || $this->person_id
+                || $this->occupant_type ) {
+            return true;
+        }
+        return false;
+    }
+
+    public function setShowAll( bool $sense ) {
+        $this->show_all = $sense;
+    }
+    
+    public function getShowAll() {
+        return $this->show_all;
+    }
+    
+    public function setSearchDate( DateTime $s_date ) {
+        $this->search_date = $s_date;
+    }
+    
+    public function getSearchDate() {
+        return $this->search_date;
+    }
+    
+    public function setPersonId( int $id ) {
+        $this->person_id = $id;
+    }
+    
+    public function getPersonId() {
+        return $this->person_id;
+    }
+
+    public function setUnitId( int $id ) {
+        $this->unit_id = $id;
+    }
+    
+    public function getUnitId() {
+        return $this->unit_id;
+    }
+
+    public function setFirstName( string $name ) {
+        $this->first_name = $name;
+    }
+    
+    public function getFirstName() {
+        return $this->first_name;
+    }
+
+    public function setLastName( string $name ) {
+        $this->last_name = $name;
+    }
+    
+    public function getLastName() {
+        return $this->last_name;
+    }
+
+    public function setOccupantType( $type ) {
+        $this->occupant_type = $type;
+    }
+    
+    public function getOccupantType() {
+        return $this->occupant_type;
+    }
+
+}
